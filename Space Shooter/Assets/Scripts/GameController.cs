0ba09b58a7efd7ebd3d843f0fec0e9b83ef2ede4
Izayami <52263008+Izayami@users.noreply.github.com>
2019-07-22 22:25:11 -0400

    Finishing touches
    Finishing the UI Text for the game and already added sound effects to prefabs.

--- Space Shooter/Assets/Scripts/GameController.cs
+++ Space Shooter/Assets/Scripts/GameController.cs
@@ -1,21 +1,52 @@
-﻿
-using UnityEngine;
+﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
-    public GameObject hazard;
-    public Vector3 spawnValues;
+public GameObject hazard;
+public Vector3 spawnValues;
+public int hazardCount;
+public float spawnWait;
+public float startWait;
+public float waveWait;
 
-    void Start ()
-    {
-        SpawnWaves ();
-    }
+public Text ScoreText;
+public Text RestartText;
+public Text GameOverText;
+private int score;
 
-    void SpawnWaves ()
-    {
-        Vector3 spawnPosition = new Vector3 (Random.Range (-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
-        Quaternion spawnRotation = Quaternion.identity;
-        Instantiate (hazard, spawnPosition, spawnRotation);
-    }
+void Start()
+{
+score = 0;
+UpdateScore();
+StartCoroutine(SpawnWaves());
+}
+
+IEnumerator SpawnWaves()
+{
+yield return new WaitForSeconds(startWait);
+while (true)
+{
+for (int i = 0; i < hazardCount; i++)
+{
+Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+Quaternion spawnRotation = Quaternion.identity;
+Instantiate(hazard, spawnPosition, spawnRotation);
+yield return new WaitForSeconds(spawnWait);
+}
+yield return new WaitForSeconds(waveWait);
+}
+}
+
+public void AddScore(int newScoreValue)
+{
+score += newScoreValue;
+UpdateScore();
+}
+
+void UpdateScore()
+{
+ScoreText.text = "Score: " + score;
+}
 }
