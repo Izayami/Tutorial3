6230b7a5b90c0b81f8ecf99db32309386cec4b87
Izayami <52263008+Izayami@users.noreply.github.com>
2019-07-28 14:30:48 -0400

    Initiate Challenge 3
    .

--- Space Shooter/Assets/Scripts/GameController.cs
+++ Space Shooter/Assets/Scripts/GameController.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
-public GameObject hazard;
+public GameObject[] hazards;
 public Vector3 spawnValues;
 public int hazardCount;
 public float spawnWait;
@@ -15,6 +15,8 @@ public float waveWait;
 public Text ScoreText;
 public Text RestartText;
 public Text GameOverText;
+
+public Text winText;
 private int score;
 private bool gameOver;
 private bool restart;
@@ -48,6 +50,7 @@ while (true)
 {
 for (int i = 0; i < hazardCount; i++)
 {
+    GameObject hazard = hazards[Random.Range (0, hazards.Length)];
 Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
 Quaternion spawnRotation = Quaternion.identity;
 Instantiate(hazard, spawnPosition, spawnRotation);
@@ -72,10 +75,16 @@ UpdateScore();
 void UpdateScore()
 {
 ScoreText.text = "Score: " + score;
+if(score >=100)
+{
+winText.text="You Win!";
+gameOver = true;
+restart = true;
+}
 }
 public void GameOver()
 {
- GameOverText.text = "Game Over!";
+ GameOverText.text = "GAME CREATED BY JEDIEL MELENDEZ";
  gameOver= true;
 
 }
