1c988f9b951c5c818ad9724b1708718c9b9d9f71
Izayami <52263008+Izayami@users.noreply.github.com>
2019-08-02 18:57:27 -0400

    Hardmode In Progress
    Hardmode in progress

--- Space Shooter/Assets/Scripts/Mover.cs
+++ Space Shooter/Assets/Scripts/Mover.cs
@@ -5,11 +5,27 @@ public class Mover : MonoBehaviour
 {
      public float speed;
 
+     public static bool hardmode;
+
      private Rigidbody rb;
 
      void Start()
      {
+          hardmode = false;
           rb = GetComponent<Rigidbody>();
           rb.velocity = transform.forward * speed;
      }
+void Update()
+{
+if (Input.GetKeyDown(KeyCode.H))
+{
+     speed = -50;
+     hardmode = true;
+}
+if (Input.GetKeyDown(KeyCode.N))
+{
+speed = -5;
+hardmode = false;
+}
+}
 }
--- Space Shooter/Assets/Scripts/PlayerController.cs
+++ Space Shooter/Assets/Scripts/PlayerController.cs
@@ -50,3 +50,4 @@ void Update()
           rb.rotation = Quaternion.Euler(0.0f, 0.0f, rb.velocity.x * -tilt);
      }
 }
+          
